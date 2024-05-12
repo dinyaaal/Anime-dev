5cc5f92d6076ebdb0cb88fb1a4db512d662b1eb7
dinyaaal <wdinyaaal@gmail.com>
2024-05-12 17:04:51 +0300

    Initial commit

---
+++ src/js/files/script.js
@@ -0,0 +1,23 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.querySelector('.anime__search');
+    const animeItems = document.querySelectorAll('.item-anime');
+
+    searchInput.addEventListener('input', function () {
+      const searchTerm = this.value.toLowerCase().trim();
+
+      animeItems.forEach(function (item) {
+        const animeName = item.querySelector('.item-anime__name').textContent.toLowerCase();
+
+        if (animeName.includes(searchTerm)) {
+          item.style.display = 'grid';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    });
+  });
