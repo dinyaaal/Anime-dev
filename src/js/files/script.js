5d226791a3fdf887cc281a08e201073d535c1a57
dinyaaal <wdinyaaal@gmail.com>
2024-06-21 16:45:12 +0300

    fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,18 +6,20 @@ import { flsModules } from "./modules.js";
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.querySelector('.anime__search');
     const animeItems = document.querySelectorAll('.item-anime');
+    if(searchInput){
 
-    searchInput.addEventListener('input', function () {
-      const searchTerm = this.value.toLowerCase().trim();
-
-      animeItems.forEach(function (item) {
-        const animeName = item.querySelector('.item-anime__name').textContent.toLowerCase();
-
-        if (animeName.includes(searchTerm)) {
-          item.style.display = 'grid';
-        } else {
-          item.style.display = 'none';
-        }
+      searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase().trim();
+  
+        animeItems.forEach(function (item) {
+          const animeName = item.querySelector('.item-anime__name').textContent.toLowerCase();
+  
+          if (animeName.includes(searchTerm)) {
+            item.style.display = 'grid';
+          } else {
+            item.style.display = 'none';
+          }
+        });
       });
-    });
+    }
   });
